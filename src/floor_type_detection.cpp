58167349700ad550c23bb94bf6a02d9c9e351b7b
Maverobot <quzhengrobot@gmail.com>
2019-09-15 16:04:14 +0200

    switched to mlpack due to lack of rnn in dlib

--- src/floor_type_detection.cpp
+++ src/floor_type_detection.cpp
@@ -1,62 +1,14 @@
-#include <dlib/dnn.h>
-#include <fstream>
-#include <iterator>
-#include <unordered_map>
-
-template <typename Out>
-void split(const std::string &s, char delim, Out result) {
-  std::stringstream ss(s);
-  std::string item;
-  while (std::getline(ss, item, delim)) {
-    *(result++) = item;
-  }
-}
-
-std::vector<std::string> split(const std::string &s, char delim) {
-  std::vector<std::string> elems;
-  split(s, delim, std::back_inserter(elems));
-  return elems;
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &o, std::vector<T> data) {
-  std::copy(data.cbegin(), data.cend(), std::ostream_iterator<T>(o, " "));
-  return o;
-}
-
-std::vector<dlib::matrix<double>> getCSV(const std::string &file,
-                                         char delimiter = ',') {
-  std::vector<dlib::matrix<double>> data;
-  std::string line, value1, value2;
-  std::ifstream f(file, std::ios::in);
-  if (!f) {
-    std::cerr << "Failed to open file !";
-    exit(1);
-  }
-  std::cout << "reading CSV file " << file << std::endl;
-  bool first = true;
-  while (getline(f, line)) {
-    if (first) {
-      first = false;
-      continue;
-    }
-    auto elements = split(line, delimiter);
-    dlib::matrix<double> line_data(1, elements.size());
-    dlib::set_all_elements(line_data, 0);
-    std::for_each(elements.cbegin(), elements.cend(),
-                  [i = 0, &line_data](auto &e) mutable {
-                    line_data(0, i) = stod(e);
-                    std::cout << e << " to " << line_data(i) << std::endl;
-                  });
-    std::cout << std::endl;
-    std::cout << line_data << std::endl << std::endl;
-    data.push_back(line_data);
-  }
-  std::cout << "Opened CSV file with " << data.size() << " lines." << std::endl;
-  return data;
-}
-
+// Includes all relevant components of mlpack.
+#include <mlpack/core.hpp>
+// Convenience.
+using namespace mlpack;
 int main(int argc, char *argv[]) {
-  getCSV(argv[1]);
+
+  // Loads the data.
+  arma::Mat<double> data;
+  data::Load(argv[1], data, true, false);
+
+  // Saves the first 10 lines
+  data::Save("data_head.csv", arma::mat(data.head_rows(10)), true);
   return 0;
 }
